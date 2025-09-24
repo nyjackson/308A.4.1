35578a45f12c25d2d8b4b9d1cb21ce816eb1f629
Nyala Jackson <nymjackson@gmail.com>
2025-09-23 21:02:06 -0400

    fetch to Axios index.js working now

--- axiosIndex.js
+++ axiosIndex.js
@@ -5,8 +5,6 @@ import {
   start,
 } from "./Carousel.js";
 
-//import axios from "axios";
-
 // The breed selection input element.
 const breedSelect = document.getElementById("breedSelect");
 // The information section div element.
@@ -20,42 +18,57 @@ const getFavouritesBtn = document.getElementById("getFavouritesBtn");
 const API_KEY =
   "live_OhZHbAsMzJK6UuuMHkxqGT9e9f4F6kgOwpvrWkEdc1lM1Al7dQaXwCwxibzzwpWy";
 
-/**
- * 1. Create an async function "initialLoad" that does the following:
- * - Retrieve a list of breeds from the cat API using fetch().
- * - Create new <options> for each of these breeds, and append them to breedSelect.
- *  - Each option should have a value attribute equal to the id of the breed.
- *  - Each option should display text equal to the name of the breed.
- * This function should execute immediately.
- */
-async function initialLoad() {
-  const breedResponse = await fetch("https://api.thecatapi.com/v1/breeds");
-  const jsonBreeds = await breedResponse.json();
+async function initialLoad(){
+  const breeds = await axios("/v1/breeds", {baseURL: "https://api.thecatapi.com", headers: {apiKey:API_KEY}})
+  console.log(breeds)
+  const jsonBreeds = breeds.data
+  for(let i = 0; i< jsonBreeds.length;i++){
+    const breedType = jsonBreeds[i]
+    const breedOption = document.createElement("option")
+    breedOption.value = breedType.id
+    breedOption.textContent = breedType.name
+    breedSelect.appendChild(breedOption)
+  }
+ start()
+}
+initialLoad()
+
+async function getCatInfo(id) {
+  const breedResponse = await axios("/v1/breeds", {baseURL: "https://api.thecatapi.com", headers: {apiKey:API_KEY}})
+  const jsonBreeds = breedResponse.data;
   for (let i = 0; i < jsonBreeds.length; i++) {
     const breedType = jsonBreeds[i];
-    const breedOption = document.createElement("option");
-    breedOption.value = breedType.id;
-    breedOption.textContent = breedType.name;
-    breedSelect.appendChild(breedOption);
+    if (id == breedType.id) {
+      return breedType;
+    }
   }
-  start();
 }
-//initialLoad();
 
-/**
- * 2. Create an event handler for breedSelect that does the following:
- * - Retrieve information on the selected breed from the cat API using fetch().
- *  - Make sure your request is receiving multiple array items!
- *  - Check the API documentation if you're only getting a single object.
- * - For each object in the response array, create a new element for the carousel.
- *  - Append each of these new elements to the carousel.
- * - Use the other data you have been given to create an informational section within the infoDump element.
- *  - Be creative with how you create DOM elements and HTML.
- *  - Feel free to edit index.html and styles.css to suit your needs, but be careful!
- *  - Remember that functionality comes first, but user experience and design are important.
- * - Each new selection should clear, re-populate, and restart the Carousel.
- * - Add a call to this function to the end of your initialLoad function above to create the initial carousel.
- */
+async function getCatPictures(event) {
+  clear();
+//   let apiLink =
+//     "https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=";
+  let breedType = event.target.value;
+//   apiLink += `${breedType}&api_key=${API_KEY}`;
+  const apiLink = await axios("/v1/images/search?limit=10", {baseURL: "https://api.thecatapi.com", headers: {apiKey:API_KEY}, breed_ids:breedType})
+
+  console.log(apiLink);
+  const jsonCats = await apiLink.data
+  for (let i = 0; i < jsonCats.length; i++) {
+    let catPic = jsonCats[i];
+    //console.log(catPic);
+    const catElt = document.createElement("img");
+    catElt.src = catPic.url;
+    const cat = createCarouselItem(catElt.src, breedType.id, catPic.id);
+    appendCarousel(cat);
+  }
+  const info = getCatInfo(breedType);
+  const jsonInfo = await info
+  const cat = craftInfoDump(jsonInfo)
+  infoDump.appendChild(cat)
+}
+
+breedSelect.addEventListener("click", getCatPictures);
 
 function craftInfoDump(info) {
   while(infoDump.firstChild){
@@ -72,49 +85,7 @@ function craftInfoDump(info) {
   frag.appendChild(description);
   return frag;
 }
-
-async function getCatInfo(id) {
-  const breedResponse = await fetch("https://api.thecatapi.com/v1/breeds");
-  const jsonBreeds = await breedResponse.json();
-  for (let i = 0; i < jsonBreeds.length; i++) {
-    const breedType = jsonBreeds[i];
-    if (id == breedType.id) {
-      return breedType;
-    }
-  }
-}
-async function getCatPictures(event) {
-  clear();
-  let apiLink =
-    "https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=";
-  let breedType = event.target.value;
-  apiLink += `${breedType}&api_key=${API_KEY}`;
-  //console.log(apiLink);
-  const catPics = await fetch(apiLink);
-  const jsonCats = await catPics.json();
-  for (let i = 0; i < jsonCats.length; i++) {
-    let catPic = jsonCats[i];
-    //console.log(catPic);
-    const catElt = document.createElement("img");
-    catElt.src = catPic.url;
-    const cat = createCarouselItem(catElt.src, breedType.id, catPic.id);
-    appendCarousel(cat);
-  }
-  const info = getCatInfo(breedType);
-  const jsonInfo = await info
-  const cat = craftInfoDump(jsonInfo)
-  infoDump.appendChild(cat)
-}
-//breedSelect.addEventListener("click", getCatPictures);
-
 /**
- * 3. Fork your own sandbox, creating a new one named "JavaScript Axios Lab."
- * Additional File: axiosIndex.js
- */
-
-
-/**
- * 
  * 4. Change all of your fetch() functions to axios!
  * - axios has already been imported for you within index.js.
  * - If you've done everything correctly up to this point, this should be simple.
@@ -123,6 +94,7 @@ async function getCatPictures(event) {
  *   by setting a default header with your API key so that you do not have to
  *   send it manually with all of your requests! You can also set a default base URL!
  */
+
 /**
  * 5. Add axios interceptors to log the time between request and response to the console.
  * - Hint: you already have access to code that does this!
